6f647125a14bdfe8fd2817eb73415b78219d7d19
INeedHelp <leonardo.bartolelli@gmail.com>
2023-04-28 19:31:34 +0200

    📦 - Lighter v0.1

--- inpython.py
+++ inpython.py
@@ -1,22 +1,23 @@
-def lighter(*files):
-    import os
-    import math
-    for file in files:
-        if os.path.isfile(file):
-            file_size = os.path.getsize(file)
-            if file_size <= 100 * 1024 * 1024:
-                print(file)
-            else:
-                file_name, file_ext = os.path.splitext(file)
-                num_parts = math.ceil(file_size / (100 * 1024 * 1024))
-                dir_name = f"{file_name}_parts"
-                os.makedirs(dir_name, exist_ok=True)
-                with open(file, 'rb') as f:
-                    for i in range(num_parts):
-                        part_path = f"{dir_name}/{file_name}_part{i + 1}{file_ext}"
-                        with open(part_path, 'wb') as part:
-                            part.write(f.read(100 * 1024 * 1024))
-                        print(part_path)
-        else:
-            print(f"File not found: {file}")
-
+#def lighter(*files):
+#    import os
+#    import math
+#    for file in files:
+#        if os.path.isfile(file):
+#            file_size = os.path.getsize(file)
+#            if file_size <= 100 * 1024 * 1024:
+#                print(file)
+#            else:
+#                file_name, file_ext = os.path.splitext(file)
+#                num_parts = math.ceil(file_size / (100 * 1024 * 1024))
+#                dir_name = f"{file_name}_parts"
+#                os.makedirs(dir_name, exist_ok=True)
+#                with open(file, 'rb') as f:
+#                    for i in range(num_parts):
+#                        part_path = f"{dir_name}/{file_name}_part{i + 1}{file_ext}"
+#                        with open(part_path, 'wb') as part:
+#                            part.write(f.read(100 * 1024 * 1024))
+#                        print(part_path)
+#        else:
+#            print(f"File not found: {file}")
+#
+#
